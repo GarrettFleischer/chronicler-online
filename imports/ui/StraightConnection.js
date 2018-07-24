bcad96bfa94f1e6b43d460eedf0b9b314e33d4fc
Garrett Fleischer <garrett.fleischer@oit.edu>
2018-07-24 16:58:29 -0700

    Added arrows to StraightConnection.

--- imports/ui/StraightConnection.js
+++ imports/ui/StraightConnection.js
@@ -6,14 +6,17 @@ export const StraightConnection = ({ points }) => {
   let d = 'M ';
   const p1 = points[0];
   const p2 = points[1];
-  const angle = Math.atan2(p2.y - p1.y, p2.x - p1.x) * 180 / Math.PI;
+  const angle = Math.atan2(p2.y - p1.y, p2.x - p1.x);
+  const dist = Math.sqrt(((p2.x - p1.x) ** 2) + ((p2.y - p1.y) ** 2));
+  const x = p1.x + Math.cos(angle) * (dist / 2);
+  const y = p1.y + Math.sin(angle) * (dist / 2);
   points.forEach((p) => {
     d += `${p.x},${p.y} `;
   });
   return (
     <g>
       <path d={d} fill="none" strokeWidth={3} stroke="white" />
-      <polygon points="2,7 0,0 11,7 0,14" transform={`translate(${(p2.x - Math.cos(angle * Math.PI / 180) * 15)}, ${p2.y - Math.sin(angle * Math.PI / 180) * 15}) rotate(${angle} 0 0) translate(-2 -7)`} fill="white" stroke="black" strokeWidth={1} />
+      <polygon points="2,7 0,0 11,7 0,14" transform={`translate(${x}, ${y}) rotate(${(angle * 180 / Math.PI)} 0 0) translate(-2 -7)`} fill="white" stroke="black" strokeWidth={1} />
     </g>
   );
 };
