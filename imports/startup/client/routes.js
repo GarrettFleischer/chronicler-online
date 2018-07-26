59e8b17e469b9241afd00b449b54e685f128b980
Garrett Fleischer <garrett.fleischer@oit.edu>
2018-07-25 20:17:05 -0700

    Added SemanticUI,

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 import { Session } from 'meteor/session';
 import { FlowRouter } from 'meteor/kadira:flow-router';
 import React from 'react';
@@ -11,13 +12,26 @@ import { Login } from '../../ui/pages/Login';
 import { Project } from '../../ui/pages/Project';
 import { Scene } from '../../ui/pages/Scene';
 
+const keyRedirectAfterLogin = 'redirectAfterLogin';
+
+Accounts.onLogin(() => {
+  const path = Session.get(keyRedirectAfterLogin);
+  Session.set(keyRedirectAfterLogin, undefined);
+  if (path) FlowRouter.go(path);
+  else FlowRouter.go('dashboard');
+});
+
+Accounts.onLogout(() => {
+  FlowRouter.go('homepage');
+});
+
 const exposed = FlowRouter.group({});
 
 const auth = FlowRouter.group({
   triggersEnter: [() => {
     if (!(Meteor.loggingIn() || Meteor.userId())) {
       const route = FlowRouter.current();
-      if (route.route.name !== 'login') Session.set('redirectAfterLogin', route.path);
+      if (route.route.name !== 'login') Session.set(keyRedirectAfterLogin, route.path);
       FlowRouter.go('login');
     }
   }],
@@ -26,49 +40,34 @@ const auth = FlowRouter.group({
 exposed.route('/', {
   name: 'homepage',
   action() {
-    mount(AppLayout, {
-      content: <Homepage />,
-    });
+    mount(AppLayout, { content: <Homepage /> });
   },
 });
 
 exposed.route('/login', {
   name: 'login',
   action() {
-    mount(AppLayout, {
-      content: <Login />,
-    });
+    mount(AppLayout, { content: <Login /> });
   },
 });
 
 auth.route('/dashboard', {
   name: 'dashboard',
   action() {
-    mount(AppLayout, {
-      content: <Dashboard />,
-    });
+    mount(AppLayout, { content: <Dashboard /> });
   },
 });
 
 auth.route('/project/:id', {
   name: 'project',
   action() {
-    mount(AppLayout, {
-      content: <Project />,
-    });
+    mount(AppLayout, { content: <Project /> });
   },
 });
 
 auth.route('/scene/:id', {
   name: 'scene',
   action() {
-    mount(AppLayout, {
-      content: <Scene />,
-    });
+    mount(AppLayout, { content: <Scene /> });
   },
 });
-
-
-// Meteor.logout(() => {
-//   FlowRouter.go('/');
-// });
